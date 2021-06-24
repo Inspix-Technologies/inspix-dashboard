efb5a6e8c5a40c94c5416b19b52fd7d0480d8eef
Putu Audi Pasuatmadi <audipasuatmadi@gmail.com>
2021-06-24 19:28:55 +0800

    develop/add submit email

--- src/views/ComingSoon.js
+++ src/views/ComingSoon.js
@@ -1,9 +1,56 @@
-import React from "react";
-import {Container, Input, FormGroup, Button} from "reactstrap";
+import React, {useRef} from "react";
+import {Container, Input, FormGroup, Button, Form} from "reactstrap";
+import {useFormik} from "formik";
+import * as Yup from "yup";
+import axios from "axios";
+import NotificationAlert from "react-notification-alert";
 
 export default function ComingSoon() {
+  const notificationRef = useRef(null);
+
+  const schema = Yup.object().shape({
+    email: Yup.string().email("please input a proper email").required(""),
+  });
+
+  const initialValues = {
+    email: "",
+  };
+
+  const formik = useFormik({
+    initialValues,
+    validationSchema: schema,
+    validateOnBlur: true,
+    validateOnChange: true,
+    onSubmit: ({email}, {setErrors, setValues}) => {
+      axios
+        .post("http://localhost:8001/api/users/subscribe", {email: email})
+        .then((res) => {
+          if (res.status === 201) {
+            setValues({email: ""});
+            notificationRef.current.notificationAlert({
+              place: "br",
+              type: "success",
+              autoDismiss: 7,
+              message: (
+                <div>
+                  <div>Email subscribed successfully</div>
+                </div>
+              ),
+            });
+          }
+        })
+        .catch((e) => {
+          if (!e.response) return;
+          setErrors({email: e.response.data.message});
+        });
+    },
+  });
+
   return (
     <div className="content">
+      <div className="react-notification-alert-container">
+        <NotificationAlert ref={notificationRef} />
+      </div>
       <Container
         fluid
         className="d-flex justify-content-center align-items-center flex-column"
@@ -22,20 +69,28 @@ export default function ComingSoon() {
         <p className="d-block text-center">
           We will email you when it's ready 😄
         </p>
-        <FormGroup
-          style={{
-            minWidth: "20rem",
-          }}
-        >
-          <Input
-            className="mt-2"
-            placeholder="example@email.com"
-            type="email"
-          />
-          <Button className="w-100 mt-2" type="submit" color="success">
-            Subscribe
-          </Button>
-        </FormGroup>
+        <Form onSubmit={formik.handleSubmit} autoComplete="off">
+          <FormGroup
+            style={{
+              minWidth: "20rem",
+            }}
+          >
+            <Input
+              className="mt-2"
+              placeholder="example@email.com"
+              type="email"
+              id="email"
+              name="email"
+              onChange={formik.handleChange}
+              invalid={formik.errors.email}
+              value={formik.values.email}
+            />
+            <p className="mt-1 text-warning">{formik.errors.email}</p>
+            <Button className="w-100 mt-2" type="submit" color="success">
+              Subscribe
+            </Button>
+          </FormGroup>
+        </Form>
       </Container>
     </div>
   );

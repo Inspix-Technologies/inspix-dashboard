0f3639091c7b5e32e1dc971f8088c5a03be9cef6
Putu Audi Pasuatmadi <audipasuatmadi@gmail.com>
2021-06-24 14:35:11 +0800

    enhance/bbox creator

--- src/libs/bounding-box/bbox-creator.js
+++ src/libs/bounding-box/bbox-creator.js
@@ -2,9 +2,15 @@ import preprocessBoundingBox from "./bbox-offset-creator";
 // import preprocessBoundingBox from "./bbox-preprocessor";
 
 const colorMap = {
-  0: "#fff719",
+  0: "#edcf47",
   1: "#0bde00",
   2: "#ff1717",
+};
+
+const labelMap = {
+  0: "Mask Wore Incorrectly",
+  1: "Mask Wore Correctly",
+  2: "Not Wearing Mask",
 };
 
 const drawBoundingBox = (imgRef, canvasRef, predictions) => {
@@ -17,7 +23,7 @@ const drawBoundingBox = (imgRef, canvasRef, predictions) => {
   canvasRef.current.width = imgRef.current.width;
   canvasRef.current.height = imgRef.current.height;
   predictions.map((detection) => {
-    // if (detection["confidence"] < 0.5) return;
+    if (detection["confidence"] < 0.45) return;
 
     const {xmin, ymin, xmax, ymax} = detection;
     const yminRelative = ymin / imgRef.current.naturalHeight;
@@ -31,18 +37,18 @@ const drawBoundingBox = (imgRef, canvasRef, predictions) => {
     );
 
     // console.log(yminRelative, xminRelative, ymaxRelative, xmaxRelative);
-
+    const textLabel = labelMap[detection["class"]];
     const color = colorMap[detection["class"]];
     ctx.strokeStyle = color;
     ctx.lineWidth = 4;
     ctx.strokeRect(x, y, width, height);
 
     ctx.fillStyle = color;
-    const textWidth = ctx.measureText(detection["name"]).width;
+    const textWidth = ctx.measureText(textLabel).width;
     const textHeight = parseInt(font, 10); // base 10
     ctx.fillRect(x, y, textWidth + 4, textHeight + 4);
     ctx.fillStyle = "#fff";
-    ctx.fillText(detection["name"], x, y + 10);
+    ctx.fillText(textLabel, x, y + 10);
   });
 };
 

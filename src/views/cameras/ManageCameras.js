15772738b3e5846885aa907fece6cb77991b0ed4
Putu Audi Pasuatmadi <audipasuatmadi@gmail.com>
2021-06-29 20:04:00 +0800

    feature/add view all user camera

---
+++ src/views/cameras/ManageCameras.js
@@ -0,0 +1,66 @@
+import React from "react";
+import {
+  Row,
+  Col,
+  Card,
+  CardHeader,
+  CardBody,
+  Button,
+  CardImg,
+  CardTitle,
+  CardText,
+} from "reactstrap";
+import {Link} from "react-router-dom";
+import useCameras from "libs/cameras/use-cameras";
+import {useUserData} from "providers/UserProvider";
+
+export default function ManageCameras() {
+  const [userData, _] = useUserData();
+
+  const cameras = useCameras(userData.id);
+
+  return (
+    <div className="content">
+      <Row>
+        <Col xs={12}>
+          <Card>
+            <CardHeader>
+              <h2 className="title">Manage Cameras</h2>
+            </CardHeader>
+            <CardBody>
+              <Row className="mx-5">
+                {cameras.length < 1 ? (
+                  <p>no cameras</p>
+                ) : (
+                  cameras.map((camera, i) => (
+                    <Col xs={4}>
+                      <Card key={i}>
+                        <CardImg
+                          top
+                          height={200}
+                          style={{objectFit: "cover"}}
+                          src={`${process.env.PUBLIC_URL}/assets/inspix-new-01.jpg`}
+                        />
+                        <CardBody>
+                          <CardTitle>{camera.name}</CardTitle>
+                          <CardText className="font-italic">
+                            {camera.source}
+                          </CardText>
+                        </CardBody>
+                      </Card>
+                    </Col>
+                  ))
+                )}
+              </Row>
+              <Link to="/admin/manage/cameras/new">
+                <Button className="btn-fill mt-2" color="primary" type="submit">
+                  New Camera
+                </Button>
+              </Link>
+            </CardBody>
+          </Card>
+        </Col>
+      </Row>
+    </div>
+  );
+}

486b2a33ea0d7d3f065b859d47bf70348b013623
Putu Audi Pasuatmadi <audipasuatmadi@gmail.com>
2021-07-04 20:53:52 +0800

    feature/add setusername api

--- src/libs/user-authentication/UserAPI.js
+++ src/libs/user-authentication/UserAPI.js
@@ -5,13 +5,34 @@ const API = axios.create({
   withCredentials: true,
 });
 
+let currentToken = "";
+
 const UserAPI = {
   getUserData: async (token) => {
+    currentToken = token;
     try {
       const response = await API.get("/", {
         headers: {Authorization: `Bearer ${token}`},
       });
       console.log(response);
+      return response;
+    } catch (e) {
+      console.error(e.response);
+      return e.response;
+    }
+  },
+
+  setUserName: async (name) => {
+    try {
+      const response = await API.post(
+        "/",
+        {
+          name: name,
+        },
+        {
+          headers: {Authorization: `Bearer ${currentToken}`},
+        }
+      );
       return response;
     } catch (e) {
       console.error(e.response);

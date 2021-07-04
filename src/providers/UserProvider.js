e56b29f39141005a49b696a7ec1e2dafacc7921a
Putu Audi Pasuatmadi <audipasuatmadi@gmail.com>
2021-07-04 20:54:19 +0800

    feature/rewrite userprovider for firebase

--- src/providers/UserProvider.js
+++ src/providers/UserProvider.js
@@ -1,34 +1,76 @@
 import React, {createContext, useState, useContext, useEffect} from "react";
-import axios from "axios";
+import firebase from "firebase";
+import UserAPI from "../libs/user-authentication/UserAPI";
+import {useHistory} from "react-router-dom";
 
 const UserContext = createContext();
+const AppContext = createContext();
+const UserTokenContext = createContext();
+
+var firebaseConfig = {
+  apiKey: "AIzaSyCa0NQQiUq2CvqtQE0xvSSPSbZ2F4zG3Ss",
+  authDomain: "inspix-technologies.firebaseapp.com",
+  projectId: "inspix-technologies",
+  storageBucket: "inspix-technologies.appspot.com",
+  messagingSenderId: "996704070792",
+  appId: "1:996704070792:web:085f6c53dc0976b9bfc33d",
+  measurementId: "G-H8ZYB943NK",
+};
 
 export default function UserProvider({children}) {
+  const history = useHistory();
+  const [isLoggedIn, setIsLoggedIn] = useState(null);
   const [userData, setUserData] = useState({});
+  const [app, setApp] = useState(null);
+  const [userToken, setUserToken] = useState({});
 
   useEffect(() => {
-    const refreshToken = localStorage.getItem("rt");
-    if (!refreshToken) return;
-    axios
-      .post("http://localhost:8001/api/token", {
-        refreshToken: refreshToken,
-      })
-      .then((res) => {
-        setUserData(res.data.user);
-      })
-      .catch((e) => {
-        console.error(e);
-      });
+    setApp(firebase.initializeApp(firebaseConfig));
+    firebase.auth().onAuthStateChanged(async (user) => {
+      if (user) {
+        const idToken = await user.getIdToken();
+        setUserToken(user);
+        try {
+          const userData = await UserAPI.getUserData(idToken);
+          if (userData.status === 403) throw userData;
+          setUserData(userData.data);
+        } catch (e) {
+          const code = e.data.code;
+          if (!code) return console.error(e.response);
+          if (code === 4) {
+            history.push("/insert-name");
+          }
+        }
+        setIsLoggedIn(true);
+        return;
+      }
+      console.log("logged out");
+      setIsLoggedIn(false);
+      setUserData({});
+      setUserToken({});
+    });
   }, []);
 
   return (
-    <UserContext.Provider value={[userData, setUserData]}>
-      {children}
-    </UserContext.Provider>
+    <UserTokenContext.Provider value={[isLoggedIn, userToken]}>
+      <UserContext.Provider value={[userData, setUserData]}>
+        <AppContext.Provider value={app}>{children}</AppContext.Provider>
+      </UserContext.Provider>
+    </UserTokenContext.Provider>
   );
 }
 
 export const useUserData = () => {
   const [userData, setUserData] = useContext(UserContext);
   return [userData, setUserData];
+};
+
+export const useAppData = () => {
+  const appData = useContext(AppContext);
+  return appData;
+};
+
+export const useUserToken = () => {
+  const [isLoggedIn, userToken] = useContext(UserTokenContext);
+  return [isLoggedIn, userToken];
 };
